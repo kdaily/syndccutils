0d1217f35e0b142d5609e173cc3435f4a70a597a
kdaily <kenneth.daily@sagebase.org>
2018-08-09 13:42:32 -0700

    change package name in setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="synapse-dcc-utility-scripts",
+    name="syndccutils",
     packages=find_packages(),
     license='Apache License, Version 2.0',
     version='1.0.0',
